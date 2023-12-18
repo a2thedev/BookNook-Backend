58d7f1f38073a3bfe84c60149227673f8396b911
a2thedev <anthonygreen048@gmail.com>
2023-12-17 20:13:02 -0500

    made changes to program.cs by adding adding own service to work with MySQL database and then created constructor that takes in application dbcontext parameter

--- BookNook/FullStackAuth_WebAPI/Models/Favorite.cs
+++ BookNook/FullStackAuth_WebAPI/Models/Favorite.cs
@@ -2,7 +2,7 @@
 {
     public class Favorite
     {
-        [key]
+        
 
         public int Id { get; set; }
         public string BookId { get; set; }
--- BookNook/FullStackAuth_WebAPI/Models/Review.cs
+++ BookNook/FullStackAuth_WebAPI/Models/Review.cs
@@ -2,7 +2,7 @@
 {
     public class Review
     {
-        [Key]
+        
 
         public int Id { get; set; }
 
