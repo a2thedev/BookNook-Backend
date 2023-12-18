51595eef8980756207b6c142fb38d1b1dfe1dab3
a2thedev <anthonygreen048@gmail.com>
2023-12-17 19:10:54 -0500

    created favorite model

--- BookNook/FullStackAuth_WebAPI/Models/Favorite.cs
+++ BookNook/FullStackAuth_WebAPI/Models/Favorite.cs
@@ -2,5 +2,14 @@
 {
     public class Favorite
     {
+        [key]
+
+        public int Id { get; set; }
+        public string BookId { get; set; }
+        public string TItle { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+
     }
 }
